e716095d90f449e23d0985ac7defcb37f167818f
Bathula Sai Vinay <saivinay.b20@iiits.in>
2023-12-09 10:19:56 +0530

    addition of attendance route

--- server/server.py
+++ server/server.py
@@ -247,3 +247,189 @@ def success():
         }, 400
 
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        try:
+            images = request.files.getlist('images')
+            rollno = request.form.get('rollno')
+            name = request.form.get('name')
+            email = request.form.get('email')
+
+            if images is None:
+                return {
+                    "message": "No images found"
+                }, 400
+            if len(images) > 10:
+                return {
+                    "message": "Maximum 10 images are allowed"
+                }, 400
+            if rollno is None or name is None or email is None:
+                return {
+                    "message": "Invalid data"
+                }, 400
+            conn = get_db_connection()
+            result = conn.execute(
+                'SELECT * FROM students WHERE student_id = ?', (rollno,)).fetchone()
+            if result is not None:
+                return {
+                    "message": "User already exists"
+                }, 400
+            for i, image in enumerate(images):
+                filename = f'uploads/upload{i}.jpg'
+                image.save(filename)
+                face = detect(filename)
+
+                user_directory = 'images/' + rollno
+                if not os.path.exists(user_directory):
+                    os.makedirs(user_directory)
+
+                if face is not None:
+                    cv2.imwrite(os.path.join(user_directory,
+                                f'{rollno}_{i}.jpg'), face)
+            with conn:
+                conn.execute('INSERT INTO students (student_id, name, email) VALUES (?, ?, ?)',
+                             (rollno, name, email))
+
+            conn.close()
+            train_thread = threading.Thread(target=train_model)
+            train_thread.start()
+            return {
+                "message": "User added successfully & training started"
+            }, 200
+        except:
+            return {
+                "message": "Internal server error"
+            }, 500
+    else:
+        return {
+            "message": "Invalid request"
+        }, 400
+
+
+def detect(image='uploads/upload.jpg', cascade='cascades/haarcascade_frontalface_default.xml'):
+    faceCascade = cv2.CascadeClassifier(cascade)
+    with open(image, 'rb') as file:
+        img_bytes = file.read()
+    nparr = np.frombuffer(img_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+    if len(faces) == 0:
+        return None
+    largest_face = max(faces, key=lambda f: f[2] * f[3])
+    x, y, w, h = largest_face
+    return img[y:y+h, x:x+w]
+
+
+def train_model():
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM students')
+        result = cursor.fetchall()
+        if result is None or len(result) == 0:
+            return
+        ids = os.listdir('images')
+        encodedList = []
+        actualids = []
+        for id in ids:
+            path = 'images/' + id
+            images = os.listdir(path)
+            for image in images:
+                actualids.append(int(id))
+                print(path + '/' + image)
+                img = cv2.imread(path + '/' + image)
+                rgb_small_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                encode = face_recognition.face_encodings(rgb_small_frame)
+                if len(encode) > 0:
+                    encode = encode[0]
+                encodedList.append(encode)
+        encode_id = [encodedList, actualids]
+        with open("recognizer/encode_id.p", "wb") as file:
+            pickle.dump(encode_id, file)
+
+
+def get_db_connection(dir='db/database.db'):
+    conn = sqlite3.connect(dir)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def class_remainder():
+    conn = get_db_connection()
+    result = conn.execute(
+        'SELECT * FROM students'
+    ).fetchall()
+
+    for row in result:
+        email = row['email']
+        try:
+            send_mailer(email, body="You have a class in 30 minutes",
+                        subject="Class Remainder")
+        except:
+            print("Error sending mail to " + email)
+    conn.close()
+
+
+def ins(date):
+    send_mailer("abhishek.h@iiits.in",
+                body="Dear Professor,\n\nYou Have an IOT class coming up tomorrow at "+date, subject="Class Remainder")
+
+
+def today_attendance():
+    conn = get_db_connection()
+    result = conn.execute(
+        'SELECT * FROM students'
+    ).fetchall()
+    todayattendance = conn.execute(
+        'SELECT * FROM attendance WHERE date = ?', (datetime.datetime.now().strftime(
+            '%Y-%m-%d'),)
+    ).fetchall()
+    stuentids = set()
+    for row in todayattendance:
+        stuentids.add(row['student_id'])
+
+    for row in result:
+        email = row['email']
+        student_id = row['student_id']
+        if student_id not in stuentids:
+            try:
+                send_mailer(email, body="You have not marked attendance today",
+                            subject="Attendance Remainder")
+            except:
+                print("Error sending mail to " + email)
+        else:
+            try:
+                send_mailer(email, body="You have marked attendance today",
+                            subject="Attendance Remainder")
+            except:
+                print("Error sending mail to " + email)
+    conn.close()
+
+
+# This is the cron job which we are improving up where class schedules are sent to students and professors
+# scheduler.add_job(class_remainder, 'cron',
+#                   day_of_week='tue', hour=15, minute=15)
+# scheduler.add_job(class_remainder, 'cron',
+#                   day_of_week='thu', hour=8, minute=15)
+# scheduler.add_job(class_remainder, 'cron',
+#                   day_of_week='fri', hour=11, minute=30)
+
+# scheduler.add_job(ins, 'cron',
+#                   day_of_week='mon', hour=15, minute=15, args=['tuesday at 3:15pm'])
+# scheduler.add_job(ins, 'cron',
+#                   day_of_week='wed', hour=8, minute=15, args=['thursday at 8:45am'])
+# scheduler.add_job(ins, 'cron',
+#                   day_of_week='thu', hour=11, minute=30, args=['friday at 12:00pm'])
+
+# scheduler.add_job(today_attendance, 'cron',
+#                   day_of_week='tue', hour=16, minute=1)
+# scheduler.add_job(today_attendance, 'cron',
+#                   day_of_week='thu', hour=9, minute=50)
+# scheduler.add_job(today_attendance, 'cron',
+#                   day_of_week='fri', hour=13, minute=5)
+
+
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
+
