bbea1db5eed47d888023db48dfe22709e588f8ac
karthikeya-io <karthikeyabethu01@gmail.com>
2023-12-09 13:44:43 +0530

    Added styling to Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Header = () => {
   return (
     <>
-      <div>
+      <div className="header">
         <Link to={"/"}>
-          <div>
+          <div className="logo">
             <span>A</span>ttendance <span>T</span>racking <span>S</span>ystem
           </div>
         </Link>
-        <ul>
-          <li>
+        <ul className="navbar">
+          <li className="nav-item">
             <a href="/dashboard">Sheet</a>
           </li>
         </ul>
