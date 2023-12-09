06103fe56ccc42bcc71af834c6387a8fc3a8561a
rahulvarma5297 <rahulvarma5297@gmail.com>
2023-12-09 11:45:10 +0530

    camera and states added

--- client/src/components/Camera/CaptureForm.jsx
+++ client/src/components/Camera/CaptureForm.jsx
@@ -1,10 +1,58 @@
-import React, { useState, useRef } from "react";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 import "./style.css";
 
 function CaptureForm() {
-  const handleSubmit = (e) => {};
+  const [start, setStart] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [rollno, setRollno] = useState("");
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (images.length !== 5) {
+      return;
+    }
+
+    const folderPath = "images/";
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("rollno", rollno);
+    images.forEach((image, index) => {
+      formData.append(
+        "images",
+        dataURItoBlob(image),
+        `${folderPath}${index}.jpg`
+      );
+    });
+
+    try {
+      await axios.post(process.env.REACT_APP_URL + "/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      alert("Data sent successfully");
+    } catch (error) {
+      console.error("Error sending data:", error);
+      alert(error.message);
+    } finally {
+      setStart(false);
+      setImages([]);
+      setProgress(0);
+      setName("");
+      setEmail("");
+      setRollno("");
+    }
+  };
+
+  const dataURItoBlob = (dataURI) => {};
 
   return (
     <div className="">
@@ -17,7 +65,12 @@ function CaptureForm() {
                   <label>Name:</label>
                 </td>
                 <td>
-                  <input type="text" required />
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
                 </td>
               </tr>
             </thead>
@@ -27,7 +80,12 @@ function CaptureForm() {
                   <label>Email:</label>
                 </td>
                 <td>
-                  <input type="email" required />
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
                 </td>
               </tr>
               <tr>
@@ -35,12 +93,68 @@ function CaptureForm() {
                   <label>Roll No:</label>
                 </td>
                 <td>
-                  <input type="number" required />
+                  <input
+                    type="number"
+                    value={rollno}
+                    onChange={(e) => setRollno(e.target.value)}
+                    required
+                  />
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
+
+        {start ? (
+          <div className="parent-camera">
+            <div className="webcam-container">
+              <Webcam
+                audio={false}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                mirrored={true}
+                width={500}
+                height={500}
+              />
+
+              <div>
+                {images.length === 5 ? (
+                  <button className="btn" type="submit">
+                    Submit Form with Images
+                  </button>
+                ) : (
+                  <button
+                    className="btn"
+                    onClick={captureImage}
+                    disabled={images.length >= 5}
+                  >
+                    Capture Image ({images.length}/5)
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="pics-1">
+              <h1>Images Captured</h1>
+              <div className="pics-2">
+                {images &&
+                  images.map((image, index) => (
+                    <img
+                      key={index}
+                      src={image}
+                      alt={`captured ${index + 1}`}
+                      style={{ width: 100, height: 100 }}
+                    />
+                  ))}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="start-btn">
+            <button className="btn" onClick={() => setStart(true)}>
+              Start Camera
+            </button>
+          </div>
+        )}
       </form>
     </div>
   );
